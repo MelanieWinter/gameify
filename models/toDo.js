42963c1beab3ba61ebb4729f02c910fade41fda1
Melanie Winter <melaniebucher830@gmail.com>
2023-12-10 12:04:24 -0800

    Adds ability to edit skill and goal from task edit page. Dynamically adds a selected filter to the option.

--- models/toDo.js
+++ models/toDo.js
@@ -31,10 +31,10 @@ const toDoSchema = new Schema({
         type: Number,
         default: 50
     },
-    dateCreated: { 
-        type: Date,
-        default: Date.now 
-    },
+    // dateCreated: { 
+    //     type: Date,
+    //     default: Date.now 
+    // },
     dueDate: { 
         type: Date
     },
